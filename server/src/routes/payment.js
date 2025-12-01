b2928590048e6136066412296d1bc5fa36ebaf1e
danh_thanh <tinhoc7649@gmail.com>
2025-12-01 14:54:41 +0700

    merge

--- server/src/routes/payment.js
+++ server/src/routes/payment.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const vnPayController = require('../app/controllers/VnPayController');
 const middlewareController = require('../app/controllers/MiddlewareController');
 
-router.post('/createqr', middlewareController.verifyToken, vnPayController.createQR);
-router.get('/check-payment-vnpay', middlewareController.verifyToken, vnPayController.checkPayment);
+router.post('/createqr', vnPayController.createVnPayQR);
 
 module.exports = router;
